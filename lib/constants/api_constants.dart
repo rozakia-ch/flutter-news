2a3c0ff83fa1f96f1c99d00b90eece9725dd897f
chusnino77 <chusnino77@gmail.com>
2021-05-11 14:25:43 +0700

    first commit

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstans {
   ApiConstans._();
   static const API_URL = "https://newsapi.org/v2/";
-  static const API_KEY = "80757d2d592740afba6d504c0965bcf8";
+  static const API_KEY = "YOUR_API_KEY";
 }
