c453227728b5f14e13c512cdb7c19c52a9fb926d
chusnino77 <chusnino77@gmail.com>
2021-05-11 14:15:09 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/flutternews/untitled/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutternews.untitled
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstans {
+  ApiConstans._();
+  static const API_URL = "https://newsapi.org/v2/";
+  static const API_KEY = "80757d2d592740afba6d504c0965bcf8";
+}
